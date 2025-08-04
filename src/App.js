be5e3427fcb3ae0c7e613718f231a0f8a2243f4e
stefanoshi <stefan.dimkovski@gmail.com>
2025-08-04 09:49:42 +0200

    new images added. style for local added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import RootLayout from "./mainPages/homePage/RootLayout";
 import HomePage from "./mainPages/homePage/HomePage";
 import About from "./mainPages/about/About";
+import Local from "./mainPages/local/Local";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
@@ -18,6 +19,10 @@ const router = createBrowserRouter([
       {
         path: "about",
         element: <About />,
+      },
+      {
+        path: "local",
+        element: <Local />,
       },
     ],
   },
